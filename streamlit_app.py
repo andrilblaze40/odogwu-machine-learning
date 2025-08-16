c353765337d0123ee07df0230e70e4fa34187ec2
andrilblaze40 <andrilizu35@gmail.com>
2025-08-16 18:13:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,23 @@
 import streamlit as st
-import pickle
+import pickle as pickle
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
 import pandas as pd
 import plotly.graph_objects as go
 import numpy as np
-
-
 def get_clean_data():
-  data = pd.read_csv("data/data.csv")
-  
-  data = data.drop(['Unnamed: 32', 'id'], axis=1)
-  
-  data['diagnosis'] = data['diagnosis'].map({ 'M': 1, 'B': 0 })
-  
-  return data
-
+    df= pd.read_csv("https://raw.githubusercontent.com/Abhishekpn09/Breast-cancer-predictor/master/.devcontainer/Cancerdataset.csv")
+    df=df.drop(['Unnamed: 32','id'], axis=1)
+    df['diagnosis'].replace({'M':1,'B':0},inplace=True)
+    return df
 
 def add_sidebar():
-  st.sidebar.header("Cell Nuclei Measurements")
-  
-  data = get_clean_data()
-  
-  slider_labels = [
+    st.sidebar.header('Cell Nuclei Details')
+    df = get_clean_data()
+    
+    slider_labels = [
         ("Radius (mean)", "radius_mean"),
         ("Texture (mean)", "texture_mean"),
         ("Perimeter (mean)", "perimeter_mean"),
@@ -52,36 +49,29 @@ def add_sidebar():
         ("Symmetry (worst)", "symmetry_worst"),
         ("Fractal dimension (worst)", "fractal_dimension_worst"),
     ]
-
-  input_dict = {}
-
-  for label, key in slider_labels:
-    input_dict[key] = st.sidebar.slider(
-      label,
-      min_value=float(0),
-      max_value=float(data[key].max()),
-      value=float(data[key].mean())
-    )
-    
-  return input_dict
-
+    input_dict ={}
+    for label, key in slider_labels:
+        input_dict[key]=st.sidebar.slider(
+            label,
+            min_value=float(0),
+            max_value=float(df[key].max()),
+            value=float(df[key].max())
+        )
+    return input_dict
 
 def get_scaled_values(input_dict):
-  data = get_clean_data()
-  
-  X = data.drop(['diagnosis'], axis=1)
-  
-  scaled_dict = {}
-  
-  for key, value in input_dict.items():
-    max_val = X[key].max()
-    min_val = X[key].min()
-    scaled_value = (value - min_val) / (max_val - min_val)
-    scaled_dict[key] = scaled_value
-  
-  return scaled_dict
-  
-
+    df=get_clean_data()
+    
+    x=df.drop(['diagnosis'], axis=1)
+    
+    scaled_dict ={}
+    
+    for key, value in input_dict.items():
+        max_val=x[key].max()
+        min_val=x[key].min()
+        scaled_value =(value-min_val)/(max_val -min_val)
+        scaled_dict[key]=scaled_value
+    return scaled_dict
 def get_radar_chart(input_data):
   
   input_data = get_scaled_values(input_data)
@@ -137,59 +127,61 @@ def get_radar_chart(input_data):
   
   return fig
 
+def train_model():
+    df = get_clean_data()
+    X = df.drop(['diagnosis'], axis=1)
+    y = df['diagnosis']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X_train, y_train)
+    
+    return model
+
+# Train model when app starts
+model = train_model()
 
 def add_predictions(input_data):
-  model = pickle.load(open("model/model.pkl", "rb"))
-  scaler = pickle.load(open("model/scaler.pkl", "rb"))
-  
-  input_array = np.array(list(input_data.values())).reshape(1, -1)
-  
-  input_array_scaled = scaler.transform(input_array)
-  
-  prediction = model.predict(input_array_scaled)
-  
-  st.subheader("Cell cluster prediction")
-  st.write("The cell cluster is:")
-  
-  if prediction[0] == 0:
-    st.write("<span class='diagnosis benign'>Benign</span>", unsafe_allow_html=True)
-  else:
-    st.write("<span class='diagnosis malicious'>Malicious</span>", unsafe_allow_html=True)
-    
-  
-  st.write("Probability of being benign: ", model.predict_proba(input_array_scaled)[0][0])
-  st.write("Probability of being malicious: ", model.predict_proba(input_array_scaled)[0][1])
-  
-  st.write("This app can assist medical professionals in making a diagnosis, but should not be used as a substitute for a professional diagnosis.")
+    scaled_input = np.array(list(input_data.values())).reshape(1, -1)
+    prediction = model.predict(scaled_input)
+
+    st.subheader("Cell cluster prediction")
+    if prediction[0] == 0:
+        st.write("Benign")
+    else:
+        st.write("Malignant")
+
+    st.write("Probability of being benign:", model.predict_proba(scaled_input)[0][0])
+    st.write("Probability of being malignant:", model.predict_proba(scaled_input)[0][1])
+    st.write("This app can assist medical professionals in making a diagnosis, but should not be used as a substitute for a professional diagnosis.")
+
+
 
 
 
 def main():
-  st.set_page_config(
-    page_title="Breast Cancer Predictor",
-    page_icon=":female-doctor:",
-    layout="wide",
-    initial_sidebar_state="expanded"
-  )
-  
-  with open("assets/style.css") as f:
-    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
-  
-  input_data = add_sidebar()
-  
-  with st.container():
-    st.title("Breast Cancer Predictor")
-    st.write("Please connect this app to your cytology lab to help diagnose breast cancer form your tissue sample. This app predicts using a machine learning model whether a breast mass is benign or malignant based on the measurements it receives from your cytosis lab. You can also update the measurements by hand using the sliders in the sidebar. ")
-  
-  col1, col2 = st.columns([4,1])
-  
-  with col1:
-    radar_chart = get_radar_chart(input_data)
-    st.plotly_chart(radar_chart)
-  with col2:
-    add_predictions(input_data)
+    st.set_page_config(
+        page_title="Breast Cancer Predictor",
+        page_icon=":female -doctor:",
+        layout="wide",
+        initial_sidebar_state="expanded"
+    )
+
+    input_data=add_sidebar()
+
+    with st.container():
+        st.title("Breast Cancer Predictor")
+        st.write("Please connect this app to your cytology lab to help diagnose breast cancer form your tissue sample. This app predicts using a machine learning model whether a breast mass is benign or malignant based on the measurements it receives from your cytosis lab. You can also update the measurements by hand using the sliders in the sidebar. ")
+
+    col1, col2 = st.columns([4,1])
+
+    with col1:  
+        radar_chart=get_radar_chart(input_data)
+        st.plotly_chart(radar_chart)
+    with col2:
+        add_predictions(input_data)
 
 
- 
+
 if __name__ == '__main__':
-  main()
+    main()
